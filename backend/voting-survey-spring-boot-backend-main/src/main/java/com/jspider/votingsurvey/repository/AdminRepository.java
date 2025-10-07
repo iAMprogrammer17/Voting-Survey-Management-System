59e75e09dfb1d61c6c580c03939e3b2dcbd9300f
Aniket Balayaan <iamprogrammer17@gmail.com>
2025-10-07 22:54:50 +0530

    Delete backend/voting-survey-spring-boot-backend-main directory

--- backend/voting-survey-spring-boot-backend-main/src/main/java/com/jspider/votingsurvey/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jspider.votingsurvey.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jspider.votingsurvey.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-
-}
